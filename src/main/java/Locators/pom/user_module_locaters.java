c2df7a5c152e590a786a98ce31a75f02df043ee3
Ayan Sengupta <sengupta.ayan90@gmail.com>
2025-06-30 20:47:12 +0530

    user module locater add started

--- src/main/java/Locators/pom/user_module_locaters.java
+++ src/main/java/Locators/pom/user_module_locaters.java
@@ -1,13 +1,54 @@
 package Locators.pom;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.vaultify.Repeat_codes.Repeatative_Codes;
 
 public class user_module_locaters extends Repeatative_Codes{
 	
-	
+	@FindBy(xpath="//input")
+	private WebElement  user_searchbar;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
+	@FindBy(xpath="")
+	private WebElement  ;
 	
 	
 	public user_module_locaters(WebDriver d){
@@ -15,7 +56,60 @@ public class user_module_locaters extends Repeatative_Codes{
 		super(d);
 		PageFactory.initElements(d, this);
 	}
-	
+	   public WebElement user_searchbar(){
+		WebElement_wait(user_searchbar);	
+		return user_searchbar;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();	
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();	
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
+		public WebElement (){
+		wait_for_theElement();
+		return ;}
 	
 	
 
